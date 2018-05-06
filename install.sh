56a1b8bb3a62983a0aa8a550ce53cf6b5d2a4673
viatoro <jarupong.wiboonchart@gmail.com>
2018-05-07 06:42:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-sudo add-apt-repository ppa:linuxuprising/java
-sudo apt-get update
-sudo apt-get install oracle-java10-installer
+#!/bin/bash
+sudo add-apt-repository ppa:linuxuprising/java -y
+sudo apt-get update -y
+sudo apt-get install oracle-java10-installer wget -y
+wget https://github.com/moomanow/minerAutoSwitch/releases/download/0.0.1/minerAutoSwitch.tar.gz
+git clone https://github.com/moomanow/buildMiner.git
